d7f58b66e1df3aa0c940b1bc5c1e67ad162d80a6
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-09-01 01:36:45 +0530

    feat: add screenshot feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,40 @@
 import logo from '@/assets/images/logo.webp'
+import domToImage from 'dom-to-image'
 
 const Header = () => {
+   const getScreenshot = () => {
+      const element = document.getElementById('tier-list')!
+
+      if (!element) {
+         return
+      }
+
+      const width = element.scrollWidth
+      const height = element.scrollHeight
+
+      domToImage
+         .toPng(element, {
+            width: width,
+            height: height,
+            style: {
+               transform: 'scale(1)',
+               transformOrigin: 'top left',
+               width: `${width}px`,
+               height: `${height}px`,
+            },
+            quality: 1,
+         })
+         .then((dataUrl) => {
+            const link = document.createElement('a')
+            link.href = dataUrl
+            link.download = 'screenshot.png'
+            link.click()
+         })
+         .catch((error) => {
+            console.error('Screenshot capture failed:', error)
+         })
+   }
+
    return (
       <header className='flex items-center justify-between'>
          <section className='flex items-center gap-4'>
@@ -12,6 +46,12 @@ const Header = () => {
                </p>
             </div>
          </section>
+         <button
+            className='bg-white/10 px-4 py-2 font-zhun text-white duration-150 hover:bg-white/20'
+            onClick={getScreenshot}
+         >
+            Screenshot
+         </button>
       </header>
    )
 }

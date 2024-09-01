b7d4f2df6feb12323a001c8ab2b195d713b2f54d
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-09-01 09:26:24 +0530

    style: minor fixes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -56,7 +56,7 @@ const Tier = ({
             <div className='grid min-w-12 place-content-center'>
                <h3 className='font-zhun text-3xl'>{tier.name}</h3>
             </div>
-            <div className='flex flex-wrap items-center gap-2'>
+            <div className='flex flex-wrap items-center gap-3'>
                {tier.ships.map((ship) => (
                   <Ship key={ship.name} ship={ship} />
                ))}

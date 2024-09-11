531d723501556ea489f03224a7758075d8476b0f
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-09-12 00:33:41 +0530

    fix: tier list shape checker did not work for less tiers in tier list

--- src/utils/type-validator.ts
+++ src/utils/type-validator.ts
@@ -1,19 +1,18 @@
 import { TierShipType, TierType } from '@/lib/types'
 
 export const isTierListArray = (value: any): value is TierType[] => {
-   return (
-      Array.isArray(value) &&
-      value.every(
-         (tier: TierType) =>
-            typeof tier === 'object' &&
-            typeof tier.name === 'string' &&
-            Array.isArray(tier.ships) &&
-            tier.ships.every(
-               (ship: TierShipType) =>
-                  typeof ship === 'object' &&
-                  typeof ship.name === 'string' &&
-                  typeof ship.image === 'string',
-            ),
-      )
+   if (!Array.isArray(value) || value.length !== 5) return false
+
+   return value.every(
+      (tier) =>
+         typeof tier === 'object' &&
+         typeof tier.name === 'string' &&
+         Array.isArray(tier.ships) &&
+         tier.ships.every(
+            (ship: TierShipType) =>
+               typeof ship === 'object' &&
+               typeof ship.name === 'string' &&
+               typeof ship.image === 'string',
+         ),
    )
 }

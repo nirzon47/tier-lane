9fcd794f8255da00e9f6021b2f1e8deefe557ed9
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-10-15 20:09:56 +0530

    feat: one click import jmm's tier lists

--- src/components/dialogs/ImportJsonDialog.tsx
+++ src/components/dialogs/ImportJsonDialog.tsx
@@ -5,6 +5,9 @@ import { useContext, useState } from 'react'
 import { isTierListArray } from '@/utils/type-validator'
 import { TierListContext } from '@/contexts/TierListContext'
 import { nanoid } from 'nanoid'
+import BattleshipsTierList from '@/assets/tier-lists/battleships.json'
+import CarriersTierList from '@/assets/tier-lists/carriers.json'
+import VanguardsTierList from '@/assets/tier-lists/vanguards.json'
 
 const ImportJsonDialog = ({
    setImportDialogOpen,
@@ -37,6 +40,25 @@ const ImportJsonDialog = ({
       }
    }
 
+   const importJimmyTierList = (type: string) => {
+      switch (type) {
+         case 'battleships':
+            importTierList?.(BattleshipsTierList)
+            break
+
+         case 'carriers':
+            importTierList?.(CarriersTierList)
+            break
+
+         case 'vanguards':
+            importTierList?.(VanguardsTierList)
+            break
+      }
+
+      setJSONTextArea('')
+      setImportDialogOpen(false)
+   }
+
    const handleImportTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
       setJSONTextArea(e.target.value)
       try {
@@ -52,28 +74,56 @@ const ImportJsonDialog = ({
    return (
       <DialogContent>
          <DialogTitle className='font-zhun'>Import JSON</DialogTitle>
-         <DialogDescription className='grid gap-2'>
-            <Textarea
-               placeholder='Paste JSON here'
-               onChange={handleImportTextarea}
-            />
+         <DialogDescription className='grid gap-2' asChild>
+            <div>
+               <Textarea
+                  placeholder='Paste JSON here'
+                  onChange={handleImportTextarea}
+               />
 
-            <span
-               className={cn(
-                  'text-sm',
-                  validJSONShape ? 'text-green-400' : 'text-red-400',
-               )}
-            >
-               {validJSONShape ? 'Valid Format' : 'Invalid Format'}
-            </span>
+               <span
+                  className={cn(
+                     'text-sm',
+                     validJSONShape ? 'text-green-400' : 'text-red-400',
+                  )}
+               >
+                  {validJSONShape ? 'Valid Format' : 'Invalid Format'}
+               </span>
 
-            <button
-               className='bg-white/10 px-4 py-2 text-white duration-150 hover:bg-white/20 disabled:cursor-not-allowed disabled:hover:bg-white/10'
-               disabled={!validJSONShape}
-               onClick={handleJSONImport}
-            >
-               Import
-            </button>
+               <button
+                  className='bg-white/10 px-4 py-2 text-white duration-150 hover:bg-white/20 disabled:cursor-not-allowed disabled:hover:bg-white/10'
+                  disabled={!validJSONShape}
+                  onClick={handleJSONImport}
+               >
+                  Import
+               </button>
+
+               <section className='mt-8 gap-3'>
+                  <p className='mb-2 font-zhun text-white'>
+                     Import Jimmy's tier lists
+                  </p>
+                  <div className='grid grid-cols-3 gap-2'>
+                     <button
+                        className='bg-white/10 px-4 py-2 font-bold text-white duration-150 hover:bg-white/20'
+                        onClick={() => importJimmyTierList('vanguards')}
+                     >
+                        Vanguards
+                     </button>
+                     <button
+                        className='bg-white/10 px-4 py-2 font-bold text-white duration-150 hover:bg-white/20'
+                        onClick={() => importJimmyTierList('carriers')}
+                     >
+                        Carriers
+                     </button>
+                     <button
+                        className='bg-white/10 px-4 py-2 font-bold text-white duration-150 hover:bg-white/20'
+                        onClick={() => importJimmyTierList('battleships')}
+                     >
+                        Battleships
+                     </button>
+                  </div>
+               </section>
+            </div>
          </DialogDescription>
       </DialogContent>
    )
